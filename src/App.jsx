86b920a69dfc0b96b80eac1be4a9dcb686b55841
AlexMoreni <alex.moreni47@gmail.com>
2023-10-30 11:20:10 -0300

    creating and styling the registration form and adding some global css

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ import {
 //Pages
 import Home from "./pages/Home/Home.jsx";
 import About from "./pages/About/About.jsx";
+import Login from "./pages/Login/Login.jsx";
+import Register from "./pages/Register/Register.jsx";
 
 //Components
 import Navbar from "./components/Navbar.jsx";
@@ -23,6 +25,8 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />}></Route>
           <Route path="/about" element={<About />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/register" element={<Register />}></Route>
         </Routes>
       </div>
       <Footer />
