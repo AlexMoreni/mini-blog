6436a8c3a6b97613ffb8a402e0597a8c6a7cda37
AlexMoreni <alex.moreni47@gmail.com>
2023-10-30 10:16:25 -0300

    configuring react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import "./App.css";
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+//Pages
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+
 function App() {
   return (
-    <>
-      <h1>Hello World!</h1>
-    </>
+    <Router>
+      <div className="container">
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
