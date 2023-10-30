371050991c992b67c1ab06e5400c376f0e353eda
AlexMoreni <alex.moreni47@gmail.com>
2023-10-30 10:37:49 -0300

    creating and styling navbar and footer

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,24 @@ import {
 } from "react-router-dom";
 
 //Pages
-import Home from "./pages/Home/Home";
-import About from "./pages/About/About";
+import Home from "./pages/Home/Home.jsx";
+import About from "./pages/About/About.jsx";
+
+//Components
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <div className="container">
         <Routes>
           <Route exact path="/" element={<Home />}></Route>
           <Route path="/about" element={<About />}></Route>
         </Routes>
       </div>
+      <Footer />
     </Router>
   );
 }
