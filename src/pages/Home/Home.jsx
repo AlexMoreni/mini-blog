ba325ed7b42d3ee7376845e56e5245c778150a14
AlexMoreni <alex.moreni47@gmail.com>
2023-11-07 06:21:01 -0300

    Creating the home, getting and rendering users' posts

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,46 @@
 import styles from "./Home.module.css";
 
+import { useNavigate, Link } from "react-router-dom";
+import { useState } from "react";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+
+//Components
+import PostDetail from "../../components/PostDetail";
+
 const Home = () => {
-  return <h1>Home</h1>;
+  const [query, setQuery] = useState("");
+  const { documents: posts, loading } = useFetchDocuments("posts");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className={styles.home}>
+      <h1>Veja os nossos posts mais recentes</h1>
+      <form onSubmit={handleSubmit} className={styles.search_form}>
+        <input type="text" placeholder="Ou busque por tags.." />
+        <button
+          className="btn btn-datk"
+          onChange={(e) => setQuery(e.target.value)}
+        >
+          Pesquisar
+        </button>
+      </form>
+      <div>
+        {loading && <p>Carregando...</p>}
+        {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Não foram encontrados posts</p>
+            <Link to="/posts/create" className="btn">
+              Criar primiro Post
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
